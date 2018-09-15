a7d3e37896e480260a88d9504e8ead947d8cb62c
Aleksandar Simic <simic.aleksandar87@gmail.com>
2018-09-15 11:23:38 +0200

    Add User model

---
+++ app/src/com/example/simpledemo/repository/model/domain/User.java
@@ -0,0 +1,105 @@
+package com.example.simpledemo.repository.model.domain;
+
+import com.example.simpledemo.repository.model.salesforce.SalesforceUser;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName(SalesforceUser.FIELD_ID) private String id;
+    @SerializedName(SalesforceUser.FIELD_USERNAME) private String username;
+    @SerializedName(SalesforceUser.FIELD_ABOUT_ME) private String aboutMe;
+    @SerializedName(SalesforceUser.FIELD_ADDRESS) private String address;
+    @SerializedName(SalesforceUser.FIELD_COMPANY_NAME) private String companyName;
+    @SerializedName(SalesforceUser.FIELD_DEPARTMENT) private String department;
+    @SerializedName(SalesforceUser.FIELD_BANNER_PHOTO_URL) private String bannerPhotoUrl;
+    @SerializedName(SalesforceUser.FIELD_MOBILE_PHONE) private String mobilePhone;
+    @SerializedName(SalesforceUser.FIELD_PHONE) private String phone;
+    @SerializedName(SalesforceUser.FIELD_PHOTO_URL) private String photoUrl;
+
+    public static User parse(SalesforceUser user, Gson gson) {
+        return gson.fromJson(user.getRawData().toString(), User.class);
+    }
+
+    private User() { }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getBannerPhotoUrl() {
+        return bannerPhotoUrl;
+    }
+
+    public void setBannerPhotoUrl(String bannerPhotoUrl) {
+        this.bannerPhotoUrl = bannerPhotoUrl;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
---
+++ app/src/com/example/simpledemo/repository/model/salesforce/SalesforceUser.java
@@ -0,0 +1,26 @@
+package com.example.simpledemo.repository.model.salesforce;
+
+
+import com.salesforce.androidsdk.smartsync.model.SalesforceObject;
+import com.salesforce.androidsdk.smartsync.util.Constants;
+
+import org.json.JSONObject;
+
+public class SalesforceUser extends SalesforceObject {
+
+    private static final String USER_OBJECT_TYPE = "User";
+    public static final String FIELD_ID = Constants.ID;
+    public static final String FIELD_USERNAME = "Username";
+    public static final String FIELD_ABOUT_ME = "AboutMe";
+    public static final String FIELD_ADDRESS = "Address";
+    public static final String FIELD_COMPANY_NAME = "CompanyName";
+    public static final String FIELD_DEPARTMENT = "Department";
+    public static final String FIELD_MOBILE_PHONE = "MobilePhone";
+    public static final String FIELD_PHONE = "Phone";
+    public static final String FIELD_PHOTO_URL = "MediumPhotoUrl";
+    public static final String FIELD_BANNER_PHOTO_URL = "BannerPhotoUrl";
+
+    public SalesforceUser(JSONObject data) {
+        super(data);
+    }
+}
